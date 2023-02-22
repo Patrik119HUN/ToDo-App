02c52d9d33f11605ab2a69702ea9e9690673e785
Patrik119HUN <tukacs.patrik77@gmail.com>
2023-02-22 21:51:01 +0100

    PHP!

---
+++ Modules/nav.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="../Styles/nav.css" />
+<div class="topnav">
+    <a class="active" id="left" href="../index.html">ToDo APP</a>
+    <a id="left" href="../Sites/galeria.html">Galéria</a>
+    <a id="left" href="../Sites/rolunk.html">Rólunk</a>
+    <span class="logo">ToDo APP</span> <!--ezt szeretném a menuben full középre igazitani csak meg nme megy-->
+    <a class="gomb" id="right">Regisztráció</a>
+    <a class="gomb" id="right" href="../Sites/bejelentkezes.html">Bejelentkezés</a>
+  </div>
