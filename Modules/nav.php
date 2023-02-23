88181369300c2b2d32c6b31c2b182d09d079b105
Patrik119HUN <tukacs.patrik77@gmail.com>
2023-02-23 13:05:11 +0100

    nav sáv

--- Modules/nav.php
+++ Modules/nav.php
@@ -1,9 +1,11 @@
 <link rel="stylesheet" href="../Styles/nav.css" />
-<div class="topnav">
-    <a class="active" id="left" href="../index.html">ToDo APP</a>
-    <a id="left" href="../Sites/galeria.html">Galéria</a>
-    <a id="left" href="../Sites/rolunk.html">Rólunk</a>
-    <span class="logo">ToDo APP</span> <!--ezt szeretném a menuben full középre igazitani csak meg nme megy-->
-    <a class="gomb" id="right">Regisztráció</a>
-    <a class="gomb" id="right" href="../Sites/bejelentkezes.html">Bejelentkezés</a>
+<nav class="topnav">
+  <p class="logo">ToDo APP</p>
+  <a class="active" id="left" href="../index.html">ToDo APP</a>
+  <a href="../Sites/galeria.html">Galéria</a>
+  <a href="../Sites/rolunk.html">Rólunk</a>
+  <div id="right">
+    <a >Regisztráció</a>
+    <a href="../Sites/bejelentkezes.html">Bejelentkezés</a>
   </div>
+</nav>
