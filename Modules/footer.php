cce2bc94647e990489b43b8862aa49af1d4b6ed2
Patrik119HUN <tukacs.patrik77@gmail.com>
2023-02-23 13:11:17 +0100

    footer.php

---
+++ Modules/footer.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="../Styles/footer.css" />
+<footer class="footer">
+    <div class="name" style="margin-left:auto">ToDo APP by Tukacs Patrik and Szegedi Bence</div>
+</footer>
