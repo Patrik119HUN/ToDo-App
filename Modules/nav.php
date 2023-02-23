3a40d851b87490adf7d9f15ca07cf942df5db2aa
Patrik119HUN <tukacs.patrik77@gmail.com>
2023-02-23 15:38:20 +0100

    router

--- Modules/nav.php
+++ Modules/nav.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="../Styles/nav.css" />
 <nav class="topnav">
-  <p class="logo">ToDo APP</p>
-  <a class="active" id="left" href="../index.html">ToDo APP</a>
-  <a href="../Sites/galeria.html">Galéria</a>
+  <a class="active" id="left" href="../index.php">ToDo APP</a>
+  <a href="../Sites/galeria.php">Galéria</a>
   <a href="../Sites/rolunk.html">Rólunk</a>
+  <p class="logo">ToDo APP</p>
   <div id="right">
     <a >Regisztráció</a>
     <a href="../Sites/bejelentkezes.html">Bejelentkezés</a>
---
+++ Sites/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="author" content="Szegedi Bence, Tukacs Patrik" />
+    <link rel="icon" type="image/x-icon" href="./képek/pipa favicon.ico" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>ToDo App</title>
+    <link rel="stylesheet" href="Styles/style.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+</head>
+
+<body>
+
+    <?php include 'Modules/nav.php' ?>
+
+
+    <div class="bg-image img1"></div>
+    <div class="bg-image img2"></div>
+    <div class="bg-image img3"></div>
+    <div class="bg-image img4"></div>
+    <div class="bg-image img5"></div>
+
+    <div class="bg-text">
+        Szeretnéd könnybben és átláthatóbban rendezni a teendőidet?
+        A ToDo app segítségével ezt egyszerűen teheted meg.
+        <br>
+        <ul>
+            <li>Átlátható heti naptár</li>
+            <li>Személyre szabhatod a megjelenítést</li>
+            <li>Jegyzeteket írhatsz a teendők mellé</li>
+        </ul>
+        Hiszünk abban, hogy ezzel az applikációval motiválni tudunk téged, hogy produktív napod legyen.
+        <br> <br>
+        <span id="use">Regisztrálj még ma és használd <span id="ingyen">INGYEN</span> a ToDo-t!</span>
+
+    </div>
+    <?php include 'Modules/footer.php' ?>
+</body>
+
+</html>
