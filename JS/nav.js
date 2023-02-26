8e4a9e0fb261f123b52bbe30aa52aedbd7343430
Patrik119HUN <tukacs.patrik77@gmail.com>
2023-02-26 19:26:34 +0100

    kész navigációs sáv

---
+++ JS/nav.js
@@ -0,0 +1,8 @@
+$(window).resize(function() {
+    if (window.innerWidth > 700) {
+      $("[id=menu]").removeClass("invisible");
+    }
+  });
+  $("#myBtn").click(function() {
+    $("[id=menu]").toggleClass("invisible");
+  });
--- Modules/nav.php
+++ Modules/nav.php
@@ -1,30 +1,20 @@
 <link rel="stylesheet" href="../Styles/nav.css" />
 
+
 <nav class="topnav">
-  <div id="menu">
-    <a href="/" class="active">ToDo APP</a>
-    <a href="/galeria">Galéria</a>
-    <a href="/rolunk">Rólunk</a>
-  </div>
-  <div class="mobile">
+  <ul>
     <p class="logo">ToDo APP</p>
     <button class="ham" id="myBtn"><img src="../SVG/menu.svg" alt="" style="width: 2rem;"></button>
-  </div>
-  <div class="asd" id="menu">
-    <a>Regisztráció</a>
-    <a href="../Sites/bejelentkezes.html">Bejelentkezés</a>
-  </div>
+  </ul>
+  <ul id="menu">
+    <li><a href="/" class="active">ToDo APP</a></li>
+    <li><a href="/galeria">Galéria</a></li>
+    <li><a href="/rolunk">Rólunk</a></li>
+  </ul>
+  <ul id="menu">
+    <li><a>Regisztráció</a></li>
+    <li><a href="../Sites/bejelentkezes.html">Bejelentkezés</a></li>
+  </ul>
 </nav>
-<script>
-  $(window).resize(function() {
-    if (window.innerWidth <  600) {
-      $("[id=menu]").addClass("invisible");
-    } else {
-      $("[id=menu]").removeClass("invisible");
-    }
-  });
-  $("#myBtn").click(function() {
-    console.log("asd");
-    $("[id=menu]").toggle("invisible");
-  });
-</script>
+
+<script src="../JS/nav.js"></script>
