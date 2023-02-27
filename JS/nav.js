2e95d9a94f28290aa405e02579dae7d9588cefbf
Szegedi Bence <benceszegedi43@gmail.com>
2023-02-27 14:42:35 +0100

    menu

--- JS/nav.js
+++ JS/nav.js
@@ -6,3 +6,10 @@ $(window).resize(function() {
   $("#myBtn").click(function() {
     $("[id=menu]").toggleClass("invisible");
   });
+
+const currantPage = window.location;
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+  if(link.href.includes(`${currantPage}`)){
+    link.classList.add('currant');
+  }
+})
