fa9e845ca577f9a0cbc39af172ceb90714166264
Patrik119HUN <tukacs.patrik77@gmail.com>
2023-03-15 17:26:13 +0100

    index

--- Modules/footer.php
+++ Modules/footer.php
@@ -1,4 +1,14 @@
 <link rel="stylesheet" href="../Styles/footer.css" />
 <footer class="footer">
-    <div class="name" style="margin-left:auto"></div>
+    <div class="name" style="margin-right:auto">
+        <p>Made by Tukacs Patrik, Szegedi Bence</p>
+        <ul style="list-style-type: none; color:yellow">
+            <li>
+                <a href="" style="text-decoration: none; color:yellow;"> Kezdőlap</a>
+            </li>
+            <li>
+                <a href=""> Galéria</a>
+            </li>
+        </ul>
+    </div>
 </footer>
