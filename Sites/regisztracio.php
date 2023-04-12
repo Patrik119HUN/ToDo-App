e810235393b9f3ea201f13d1843d7c3fe6229ecb
Szegedi Bence <benceszegedi43@gmail.com>
2023-04-12 12:29:14 +0200

    regisztracio

--- Sites/regisztracio.php
+++ Sites/regisztracio.php
@@ -5,44 +5,40 @@
 
   if (isset($_GET["register"])) {   // csak azután dolgozzuk fel az űrlapot, miután az el lett küldve
 
-    if (!isset($_GET["id"]) && trim($_GET["id"]) === "")
-      $hibak[] = "A felhasználó név megadása kötelező!";
-
-    if (!isset($_GET["surname"]) && trim($_GET["surname"]) === "")
-      $hibak[] = "A vezetéknév megadása kötelező!";
-
-    if (!isset($_GET["forename"]) && trim($_GET["forename"]) === "")
-      $hibak[] = "A keresztnév megadása kötelező!";
-
-    if (!isset($_GET["email"]) && trim($_GET["email"]) === "")
-      $hibak[] = "A e-mail cím megadása kötelező!";
-
-    if (!isset($_GET["psw"]) || trim($_GET["psw"]) === "" || !isset($_GET["psw_n"]) || trim($_GET["psw_n"]) === "")
-      $hibak[] = "A jelszó és az ellenőrző jelszó megadása kötelező!";
-
-    if (!isset($_GET["birthday"]) || trim($_GET["birthday"]) === "")
-      $hibak[] = "Az életkor megadása kötelező!";
-
-
     $id = $_GET["id"];
     $vezeteknev = $_GET["surname"];
-    $keresztnev = $_GET["forename"];
+    $keresztnev = $_GET["forname"];
     $email = $_GET["email"];
     $jelszo = $_GET["psw"];
     $jelszo2 = $_GET["psw_n"];
     $eletkor = $_GET["birthday"];
-    // túl rövid jelszó
 
-    if (strlen($jelszo) < 5)
+    
+    if (!isset($vezeteknev) || trim($vezeteknev) === "")
+      $hibak[] = "A vezetéknév megadása kötelező!";
+
+    if (!isset($keresztnev) || trim($keresztnev) === "")
+      $hibak[] = "A keresztnév megadása kötelező!";
+    
+    if (!isset($id) || trim($id) === "")
+      $hibak[] = "A felhasználó név megadása kötelező!";
+
+    if (!isset($eletkor) || trim($eletkor) === "")
+      $hibak[] = "Az születési év megadása kötelező!"; 
+
+    if (!isset($email) || trim($email) === ""){
+      $hibak[] = "A e-mail cím megadása kötelező!";
+    }else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $hibak[] = "Érvénytelen e-mail cím";
+    }
+
+    if (!isset($jelszo) || trim($jelszo) === "" || !isset($jelszo2) || trim($jelszo2) === ""){
+      $hibak[] = "A jelszó és az ellenőrző jelszó megadása kötelező!";
+    }else if (strlen($jelszo) < 5) {
       $hibak[] = "A jelszónak legalább 5 karakter hosszúnak kell lennie!";
-
-    // a két jelszó nem egyezik
-    if ($jelszo !== $jelszo2)
+    }else if ($jelszo !== $jelszo2) {
       $hibak[] = "A jelszó és az ellenőrző jelszó nem egyezik!";
-
-    // 18 év alatti életkor
-    if ($eletkor < 18)
-      $hibak[] = "Csak 18 éves kortól lehet regisztrálni!";
+    }    
 
     foreach ($fiokok as $fiok) {
       if ($fiok["id"] === $id)
@@ -66,19 +62,19 @@
         <h1>Regisztráció</h1>
         <div style="display:flex; gap:2rem">
           <div>
-            <label for="surename"><b>Vezeték név</b></label>
+            <label for="surname"><b>Vezetéknév</b></label>
             <input class="shadow" type="text" placeholder="Jankó" name="surname" value="<?php if (isset($_GET['surname'])) echo $_GET['surname']; ?>" />
           </div>
           <div>
-            <label for="forename"><b>Kereszt név</b></label>
-            <input class="shadow" type="text" placeholder="Pista" name="forename" value="<?php if (isset($_GET['forename'])) echo $_GET['forename']; ?>" />
+            <label for="forname"><b>Keresztnév</b></label>
+            <input class="shadow" type="text" placeholder="Pista" name="forname" value="<?php if (isset($_GET['forname'])) echo $_GET['forname']; ?>" />
           </div>
         </div>
         <label for="id"><b>Felhasználónév</b></label>
         <input class="shadow" type="text" placeholder="jankópityu129" name="id" value="<?php if (isset($_GET['id'])) echo $_GET['id']; ?>" />
         <label for="birthday"><b>Születési dátum: </b></label>
         <input class="shadow" type="date" id="birthday" name="birthday" value="<?php if (isset($_GET['birthday'])) echo $_GET['birthday']; ?>" />
-
+        <br>
         <label for="email"><b>Email</b></label>
         <input class="shadow" type="text" placeholder="jankopisti@valami.com" name="email" value="<?php if (isset($_GET['email'])) echo $_GET['email']; ?>" />
 
@@ -94,7 +90,7 @@
           echo "<p style='color:green; font-size:1rem;'>Sikeres regisztráció!</p>";
         } else {                                // az esetleges hibákat kiírjuk egy-egy bekezdésben
           foreach ($hibak as $hiba) {
-            echo "<p style='color:green; font-size:1rem;'>$hiba</p>";
+            echo "<p style=' color:red; font-size:1.3rem;'>$hiba</p>";
           }
         }
         ?>
