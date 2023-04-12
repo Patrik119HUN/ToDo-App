7db054b24f21f17b57a32c78558013292dc45b31
Patrik119HUN <tukacs.patrik77@gmail.com>
2023-04-12 11:09:53 +0200

    fel

--- Sites/teendok.php
+++ Sites/teendok.php
@@ -1,7 +1,7 @@
-<div style="min-height:100vh;max-height:fit-content; background-color: yellow; padding: 1rem;padding-bottom: 5rem; display:flex;flex-direction: row; gap:1rem;justify-content: space-between;">
+<!-- <div style="min-height:100vh;max-height:fit-content; background-color: yellow; padding: 1rem;padding-bottom: 5rem; display:flex;flex-direction: row; gap:1rem;justify-content: space-between;">
+</div> -->
     <?php
     include 'Components/TaskManager/TaskManager.php';
     $taskManager = new TaskManager();
     $taskManager->render();
     ?>
-</div>
